be1f8e9250119d38eda29f0b2f011198cb02117a
andrewthechilldude <117551533+andrewthechilldude@users.noreply.github.com>
2025-01-17 21:06:12 -0600

    Added scripts
    allegedly

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Basic Stats")]
+    [SerializeField] private float runSpeed;
+
+    private void FixedUpdate() {
+        
+    }
+
+
+}
